54c36e74b1c8fbe4e9075bbdc54962182098431e
stephanie-115 <stephanie.terese@gmail.com>
2023-09-19 18:06:44 -0400

    Webpack and server file set up

---
+++ server/server.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const express = require('express');
+// const userRouter = require('../routes/userRouter');
+// const recipeRouter = require('../routes/recipeRouter')
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('../webpack.config');
+
+const app = express();
+const compiler = webpack(webpackConfig);
+
+// Use middleware for webpack
+app.use(
+    webpackDevMiddleware(compiler, {
+        publicPath: webpackConfig.output.publicPath,
+    })
+);
+
+// Use hot middleware
+app.use(webpackHotMiddleware(compiler));
+
+const port = 3000;
+
+//middleware to parse json & url-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes:
+// app.use('/user', userRouter);
+// app.use('/recipe', recipeRouter)
+
+///serve main index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../index.html'));
+  });  
+
+//serve css & js assets
+app.get('/css/style.css', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../src/index.css'));
+});
+app.get('/js/index.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../src/index.js'));
+});
+
+// 404 error handling - this should be near the end, right before the general error handler
+app.use('*', (req, res) => {
+    res.status(404).json({ error: 'This page does not exist' });
+});
+
+//general error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500);
+
+    if (process.env.NODE_ENV === 'production') {
+        res.json({ error: 'An error occurred' });
+    } else {
+        res.json({ error: err.message, stack: err.stack });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    entry: {
+        app: [
+            './src/index.js',
+            'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            fallback: 'file-loader',
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000
+    }
+    
+};
