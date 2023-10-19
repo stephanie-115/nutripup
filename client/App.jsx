3103f968a389c67cd00af68b6087226d08239f13
stephanie-115 <stephanie.terese@gmail.com>
2023-10-19 11:01:33 -0400

    setting up App file

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function App() {
-  
+
+  return (
+  <div className="app">
+        <Navbar />
+        <UserProfile />
+  </div>
+  )
 }
