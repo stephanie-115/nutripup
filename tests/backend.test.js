4deec0669f7df386663fdb3cfc977a0bc4364dfb
stephanie-115 <stephanie.terese@gmail.com>
2023-12-11 20:21:52 -0500

    Unit Testing

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,30 +1,30 @@
-const request = require('supertest');
-const express = require('express');
-const dogController = require('../server/controllers/dogController');
-const nutrientCalcs = require('../server/utils/nutrientCalcs');
-jest.mock('../server/utils/nutrientCalcs');
+const request = require("supertest");
+const express = require("express");
+const dogController = require("../server/controllers/dogController");
+const nutrientCalcs = require("../server/utils/nutrientCalcs");
+jest.mock("../server/utils/nutrientCalcs");
 
-describe('dogController tests', () => {
+describe("dogController tests", () => {
   let app;
 
   beforeAll(() => {
     app = express();
     app.use(express.json());
     app.use((req, res, next) => {
-      req.user = { id: 1};
+      req.user = { id: 1 };
       next();
     });
-    app.post('/dog/addDog', dogController.addDog);
+    app.post("/dog/addDog", dogController.addDog);
   });
 
   beforeEach(() => {
-    jest.spyOn(nutrientCalcs, 'calculateCalories').mockReturnValue(500);
-    jest.spyOn(nutrientCalcs, 'calculateProtein').mockReturnValue(50);
-    jest.spyOn(nutrientCalcs, 'calculateCarbs').mockReturnValue(20);
-    jest.spyOn(nutrientCalcs, 'calculateFat').mockReturnValue(30);
-  })
+    jest.spyOn(nutrientCalcs, "calculateCalories").mockReturnValue(500);
+    jest.spyOn(nutrientCalcs, "calculateProtein").mockReturnValue(50);
+    jest.spyOn(nutrientCalcs, "calculateCarbs").mockReturnValue(20);
+    jest.spyOn(nutrientCalcs, "calculateFat").mockReturnValue(30);
+  });
 
-  it('should add a dog successfully to the db', async () => {
+  it("should add a dog successfully to the db", async () => {
     const mockDog = {
       dogName: "Stanley",
       dogBreed: "English Bulldog",
@@ -34,7 +34,7 @@ describe('dogController tests', () => {
       allergies: "Chicken, Turkey, Sweet Potato, Salmon",
     };
 
-    const response = (await request(app).post('/dog/addDog').send(mockDog));
+    const response = await request(app).post("/dog/addDog").send(mockDog);
     expect(response.statusCode).toBe(201);
-  })
-})
+  });
+});
